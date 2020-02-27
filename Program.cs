65341806bf616f6c9915bf54d23b91b2c7203671
wladweb <wladwebwork@gmail.com>
2020-02-27 18:24:37 +0300

    map read & draw

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Pacman
 {
@@ -10,6 +7,38 @@ namespace Pacman
     {
         static void Main(string[] args)
         {
+            char[,] map = ReadMap("map1");
+            DrawMap(map);
+        }
+
+        static void DrawMap(char[,] map)
+        {
+            for (int i = 0, lenOuter = map.GetLength(0); i < lenOuter; i++)
+            {
+                for (int j = 0, lenInner = map.GetLength(1); j < lenInner; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] ReadMap(string mapName)
+        {
+            string mapFilePath = $"../../maps/{mapName}.txt";
+            string[] fileLines = File.ReadAllLines(mapFilePath);
+
+            char[,] map = new char[fileLines.Length, fileLines[0].Length];
+
+            for (int i = 0, mapRows = map.GetLength(0); i < mapRows; i++)
+            {
+                for (int j = 0, mapCols = map.GetLength(1); j < mapCols; j++)
+                {
+                    map[i, j] = fileLines[i][j];
+                }
+            }
+
+            return map;
         }
     }
 }
